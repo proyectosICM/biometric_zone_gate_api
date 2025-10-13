5bf3a57c7f677f809f629b566ed891922646bb6f
Eduardo Aguilar <es123y123@gmail.com>
2025-10-12 20:32:00 -0500

    c

--- src/main/java/com/icm/biometric_zone_gate_api/config/WebSocketServerConfig.java
+++ src/main/java/com/icm/biometric_zone_gate_api/config/WebSocketServerConfig.java
@@ -3,9 +3,11 @@ package com.icm.biometric_zone_gate_api.config;
 import jakarta.websocket.server.ServerEndpoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
+@Profile("!test")
 public class WebSocketServerConfig {
 
     @Bean
--- src/test/java/com/icm/biometric_zone_gate_api/BiometricZoneGateApiApplicationTests.java
+++ src/test/java/com/icm/biometric_zone_gate_api/BiometricZoneGateApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.icm.biometric_zone_gate_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BiometricZoneGateApiApplicationTests {
 
 	@Test
