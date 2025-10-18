b06741506ce0e6640a201d1daba65e2c9b7b5ebb
Eduardo Aguilar <es123y123@gmail.com>
2025-10-17 23:16:12 -0500

    c

--- src/main/java/com/icm/biometric_zone_gate_api/config/WebSocketServerConfig.java
+++ src/main/java/com/icm/biometric_zone_gate_api/config/WebSocketServerConfig.java
@@ -2,6 +2,7 @@ package com.icm.biometric_zone_gate_api.config;
 
 import com.icm.biometric_zone_gate_api.websocket.DeviceMessageHandler;
 import com.icm.biometric_zone_gate_api.websocket.DeviceWebSocketHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -9,14 +10,15 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
 @Configuration
 @EnableWebSocket
+@RequiredArgsConstructor
 public class WebSocketServerConfig implements WebSocketConfigurer {
 
     private final DeviceMessageHandler deviceMessageHandler;
-
+/*
     public WebSocketServerConfig(DeviceMessageHandler deviceMessageHandler) {
         this.deviceMessageHandler = deviceMessageHandler;
     }
-
+*/
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(new DeviceWebSocketHandler(deviceMessageHandler), "/")
