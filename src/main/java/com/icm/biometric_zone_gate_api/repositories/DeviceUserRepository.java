57735e782eb3eac052dee01e032cc65b0fed7bb3
Eduardo Aguilar <es123y123@gmail.com>
2025-10-18 20:55:41 -0500

    c

--- src/main/java/com/icm/biometric_zone_gate_api/repositories/DeviceUserRepository.java
+++ src/main/java/com/icm/biometric_zone_gate_api/repositories/DeviceUserRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface DeviceUserRepository extends JpaRepository<DeviceUserModel, Long> {
-    List<DeviceUserModel> findByUserId(Long userId);
-    List<DeviceUserModel> findByDeviceId(Long deviceId);
+    //List<DeviceUserModel> findByUserId(Long userId);
+    //List<DeviceUserModel> findByDeviceId(Long deviceId);
     Optional<DeviceUserModel> findByDeviceIdAndEnrollId(Long deviceId, Integer enrollId);
 }
