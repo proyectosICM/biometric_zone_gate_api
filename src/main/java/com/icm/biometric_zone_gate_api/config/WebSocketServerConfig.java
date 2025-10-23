1fedf9d3b00f73846505d1c31843d83736fb3594
Eduardo Aguilar <es123y123@gmail.com>
2025-10-23 17:27:21 -0500

    c

--- src/main/java/com/icm/biometric_zone_gate_api/config/WebSocketServerConfig.java
+++ src/main/java/com/icm/biometric_zone_gate_api/config/WebSocketServerConfig.java
@@ -3,10 +3,12 @@ package com.icm.biometric_zone_gate_api.config;
 import com.icm.biometric_zone_gate_api.websocket.DeviceMessageHandler;
 import com.icm.biometric_zone_gate_api.websocket.DeviceWebSocketHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
@@ -20,5 +22,15 @@ public class WebSocketServerConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(deviceWebSocketHandler, "/ws")
                 .setAllowedOrigins("*");
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean webSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        // Los dispositivos pueden enviar plantillas biométricas grandes codificadas en Base64.
+        // Aumentamos el buffer permitido para evitar cierres del canal (código 1009) por mensajes largos.
+        container.setMaxTextMessageBufferSize(4 * 1024 * 1024); // 4 MB
+        container.setMaxBinaryMessageBufferSize(4 * 1024 * 1024);
+        return container;
     }
 }
